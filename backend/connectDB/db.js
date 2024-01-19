f44fb075fb297620e022f654c61d949ef9df9c00
Madan k <partagowda15@gmail.com>
2024-01-19 07:15:06 +0530

    mongoose connection

--- backend/connectDB/db.js
+++ backend/connectDB/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async()=>{
     try {
-        const con = await mongoose.connect(process.env.MONGOOSE_URL,{ useNewUrlParser: true, useUnifiedTopology: true })
+        const con = await mongoose.connect(process.env.MONGOOSE_URL,{ useUnifiedTopology: true, })
         console.log(`MongoDB Connected : ${con.connection.host}`)
     } catch (error) {
         console.log('ERROR :', error.message)
