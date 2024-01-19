23ac9140a86ab56e462de47b8ee592d4430fd144
Madan k <partagowda15@gmail.com>
2024-01-19 07:11:56 +0530

    initial commit

--- frontend/src/utils/credential.js
+++ frontend/src/utils/credential.js
@@ -1,3 +1,5 @@
+// export const BASE_BACKEND_URL = 'https://cottage-works.onrender.com'
+
 export const BASE_BACKEND_URL = 'http://localhost:5000';
 
 export const {token,role,_id,name} = JSON.parse(localStorage.getItem("user")) || {token:null}
